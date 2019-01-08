209e44e03c018708061397e25eaf22f4c6079252
evansj <evansj@email.chop.edu>
2019-01-08 14:45:15 -0500

    Add snptest pvals to annotation table.

--- src/rules/sf_ann.py
+++ src/rules/sf_ann.py
@@ -56,18 +56,22 @@ rule parse_vcf_ann:
 rule ann_plink_assoc:
     input:
         a = DATA + 'interim/plink_assoc_fmt/{group}/{pop}.assoc',
-        st = DATA + 'interim/{group}_snptest_final/snptest.out',
+        st = DATA + 'interim/{pop}_snptest_final/snptest.out',
         g = DATA + 'interim/ibd_gwas.{pop}.assoc',
         s = DATA + "interim/variants/snpeff_parsed/{pop}"
     output:
         o = DATA + 'interim/plink_assoc_fmt_ann/{group}/{pop}.assoc'
     run:
         dat = pd.read_csv(input.s, sep='\t')
+        st_cols = {'frequentist_add_pvalue':'P_snptest', 'alleleA':'A1_snptest', 'alleleB':'A2_snptest', 'all_OR':'OR_snptest'}
+        snptest = pd.read_csv(input.st, sep='\t')[['frequentist_add_pvalue', 'chromosome', 'position', 'alleleA', 'alleleB', 'cases_maf', 'controls_maf', 'all_OR']].rename(columns=st_cols).fillna(1)
+        snptest.loc[:, 'SNP'] = snptest.apply(lambda row: str(row['chromosome']) + ':' + str(row['position']), axis=1)
         assoc = pd.read_csv(input.a, sep='\t').rename(columns={'A1':'A1_plink', 'A2':'A2_plink', 'P':'P_plink', 'OR':'OR_plink'})
         assoc.loc[:, 'SNP'] = assoc.apply(lambda row: str(row['CHR']) + ':' + str(row['BP']), axis=1)
+        full_assoc = pd.merge(assoc, snptest, on='SNP', how='left')
         adult_gwas = pd.read_csv(input.g, delim_whitespace=True).rename(columns={'SNP':'rs', 'A1':'A1_adult', 'A2':'A2_adult', 'OR':'OR_adult', 'PVAL':'PVAL_adult', 'SE':'SE_adult'})
         adult_gwas.loc[:, 'SNP'] = adult_gwas.apply(lambda row: str(row['CHR']) + ':' + str(row['BP']), axis=1)
-        m1 = pd.merge(assoc, adult_gwas, on='SNP', how='left').fillna('')
+        m1 = pd.merge(full_assoc, adult_gwas, on='SNP', how='left').fillna('')
         crit = m1.apply(lambda row: row['P_plink']<1e-5 or row['A1_adult']!='', axis=1)
         pd.merge(m1[crit], dat, how='left', on='SNP').to_csv(output.o, index=False, sep='\t')
 
@@ -80,6 +84,8 @@ rule gene_assoc:
         df = pd.read_csv(input.a, sep='\t')
         df.loc[:, 'gene'] = df.apply(lambda row: row['eff'].split('ANN=')[1].split('|')[3], axis=1)
         cols = ['SNP', 'gene', 'A1_plink', 'A2_plink', 'P_plink', 'OR_plink',
+                'A1_snptest', 'A2_snptest', 'P_snptest', 'OR_snptest',
+                'cases_maf', 'controls_maf',
                 'A1_adult', 'A2_adult', 'PVAL_adult', 'OR_adult',
                 'A1_vcf', 'A2_vcf', 'eff']
         df[cols].to_csv(output.o, index=False, sep=',')
